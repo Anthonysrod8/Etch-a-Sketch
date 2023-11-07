e3f22ecd69cf6e90dcfb595e3681d864df5798a7
Anthony Rodriguez <anthonyo8@hotmail.com>
2023-11-06 22:25:44 -0800

    started from scratch with more understandable logic

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const container = document.querySelector('#container');
 
-let maker = (rows, cols) => {
-    container.style.setProperty('--grid-rows', rows)
-    container.style.setProperty('--grid-cols', cols)
-    for (let i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement('div');
-        container.appendChild(cell).className = 'grid-item';
-        cell.innerText = (i + 1)
+const rowAmount = 0;
 
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    let choice = Number(prompt('how many squares you want?'))
+
+    if (choice > 100) {
+        choice = Number(prompt('Make sure its under under 101'))
     }
+    const newContainer = document.querySelector('.newContainer')
+
+    if (!newContainer) {
+        maker(choice)
+    } else {
+        newContainer.remove()
+        maker(choice)
+    }
+    
+})
+
+let maker = (amount) => {
+    const newContainer = document.createElement('div')
+    newContainer.classList.add('newContainer')
+    for (let i = 0; i < amount; i++) {
+        const rows = document.createElement('div')
+        rows.classList.add('rows')
+
+        for (let j = 0; j < amount; j++) {
+            const rowBox = document.createElement('div')
+            rowBox.classList.add('rowBox')
+            rows.appendChild(rowBox)
+            rowBox.addEventListener('mouseover', () => {
+                rowBox.style.background = 'blue';
+            })
+        }
+        newContainer.appendChild(rows)
+    }
+    container.appendChild(newContainer)
 }
-maker(16, 16)
+
+
+maker(rowAmount)
